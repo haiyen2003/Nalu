512e9d546f82d076a590feb2abf1361c5e6469e5
hai yen <nhyen.93@gmail.com>
2022-10-26 10:14:49 -0700

    add validation in Form

--- app/forms/route_form.py
+++ app/forms/route_form.py
@@ -8,10 +8,20 @@ from app.models import Route
 
 sportType_options = ['Surfing', 'Swimming']
 
+def name_validation(form, field):
+    name = field.data
+    if len(name) < 5 or len(name) > 200:
+        raise ValidationError("Name must be more than 5 characters and less than 200 characters")
+
+def description_validation(form, field):
+    des = field.data
+    if len(des) < 20 or len(des) > 600:
+        raise ValidationError("Description must be more than 20 characters and less than 600 characters")
+
 class RouteForm(FlaskForm):
-    name = StringField("Route Name", validators = [DataRequired()])
+    name = StringField("Route Name", validators = [DataRequired(), name_validation])
     distance = DecimalField('Distance', validators=[DataRequired()])
-    description = TextAreaField('Route Description', validators=[DataRequired()])
+    description = TextAreaField('Route Description', validators=[DataRequired(), description_validation])
     startPoint = StringField('Start Point', validators=[DataRequired()])
     endPoint = StringField ('End Point', validators=[DataRequired()])
     duration = StringField('Duration', validators= [DataRequired()])
--- app/forms/workout_form.py
+++ app/forms/workout_form.py
@@ -13,13 +13,23 @@ def image_validation(form, field):
     if not img[-3:] == 'jpg' and (not img[-3:] == 'png') and img[-4:] != 'jpeg' and img[-4:] != 'webp' and img[-3:] != 'gif' and img[-3:] != 'svg':
         raise ValidationError("Input must be a valid Image Url")
 
+def name_validation(form, field):
+    name = field.data
+    if len(name) < 5 or len(name) > 200:
+        raise ValidationError("Name must be more than 5 characters and less than 200 characters")
+
+def description_validation(form, field):
+    des = field.data
+    if len(des) < 20 or len(des) > 600:
+        raise ValidationError("Description must be more than 20 characters and less than 600 characters")
+
 equipment_option = ['surfboard', 'paddleboard']
 
 class WorkoutForm(FlaskForm):
-    name = StringField("Route Name", validators = [DataRequired()])
+    name = StringField("Route Name", validators = [DataRequired(), name_validation])
     distance = DecimalField('Distance', validators=[DataRequired()])
-    description = TextAreaField('Route Description', validators=[DataRequired()])
-    image = StringField('Image URL', validators=[DataRequired(), image_validation ])
+    description = TextAreaField('Route Description', validators=[DataRequired(), description_validation])
+    image = StringField('Image URL', validators=[image_validation ])
     startPoint = StringField('Start Point', validators=[DataRequired()])
     endPoint = StringField ('End Point', validators=[DataRequired()])
     duration = StringField('Duration', validators= [DataRequired()])
