537a49ffa26574c3880aa84d1c6f1132e5b9e8e4
hai yen <nhyen.93@gmail.com>
2022-10-28 00:29:09 -0700

    session route

--- app/api/session_routes.py
+++ app/api/session_routes.py
@@ -1,103 +1,100 @@
 from datetime import datetime
 from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.forms import WorkoutForm
-from app.models import Workout, db, User, Route
+from app.forms import SessionForm
+from app.models import Session, db, User, Spot
 from app.api.auth_routes import validation_errors_to_error_messages
 
-workout_routes = Blueprint('workouts', __name__)
+session_routes = Blueprint('sessions', __name__)
 now = datetime.now()
 
-# get all Workouts:
-@workout_routes.route('/')
-def workouts():
-    workouts = Workout.query.all()
-    return {"workout": [workout.to_dict() for workout in workouts]}
+# get all sessions:
+@session_routes.route('/')
+def sessions():
+    sessions = Session.query.all()
+    return {"Session": [session.to_dict() for session in sessions]}
 
-# get all workouts by current user:
-@workout_routes.route('/my-workouts')
+# get all sessions by current user:
+@session_routes.route('/my-sessions')
 @login_required
-def my_workouts():
+def my_sessions():
     currentUserId = current_user.id
-    workouts = Workout.query.filter(Workout.userId == currentUserId).all()
-    return {'myWorkouts': [workout.to_dict() for workout in workouts]}
+    sessions = Session.query.filter(Session.userId == currentUserId).all()
+    return {'mySessions': [session.to_dict() for session in sessions]}
 
 # get specific workout by id:
-@workout_routes.route('/<int:id>')
-def workout(id):
-    workout = Workout.query.get(id)
+@session_routes.route('/<int:id>')
+def session(id):
+    workout = Session.query.get(id)
     if workout is None:
-        return {'message': 'Workout not found'}
+        return {'message': 'Session not found'}
 
     else:
-        return workout.to_dict()
+        return session.to_dict()
 
-# create a workout:
-@workout_routes.route('/new', methods=["POST"])
+# create a session:
+@session_routes.route('/new', methods=["POST"])
 @login_required
 def create_workout():
-    form = WorkoutForm()
+    form = SessionForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        newWorkout = Workout(
+        newSession = Session(
             name = form.data['name'],
             equipment = form.data['equipment'],
             description = form.data['description'],
             image = form.data['image'],
             startTime = form.data['startTime'],
             endTime = form.data['endTime'],
-            sportType = form.data['sportType'],
             createAt = now,
             updateAt = now
         )
 
-        db.session.add(newWorkout)
+        db.session.add(newSession)
         db.session.commit()
-        return newWorkout.to_dict()
+        return newSession.to_dict()
 
     return {"errors" : validation_errors_to_error_messages(form.errors)}, 400
 
-# Update a workout:
-@workout_routes.route('/<int:id>', methods = ['PUT'])
+# Update a session:
+@session_routes.route('/<int:id>', methods = ['PUT'])
 @login_required
 def update_workout():
-    form = WorkoutForm()
+    form = SessionForm()
     form['csrf_token'].data = request.cookies['csrf_token']
-    thisWorkout = Workout.query.get(id)
-    if thisWorkout is None:
-        return {'errors': 'Workout not found'}, 404
+    thisSession = Session.query.get(id)
+    if thisSession is None:
+        return {'errors': 'Session not found'}, 404
 
-    if thisWorkout.userId != current_user.id:
+    if thisSession.userId != current_user.id:
         return{'errors': 'Unauthorized'}, 403
 
     if form.validate_on_submit():
-        thisWorkout.name = form.data['name'],
-        thisWorkout.distance = form.data['distance'],
-        thisWorkout.description = form.data['description'],
-        thisWorkout.startPoint = form.data['startPoint'],
-        thisWorkout.endPoint = form.data['endPoint'],
-        thisWorkout.duration = form.data['duration'],
-        thisWorkout.sportType = form.data['sportType'],
-        thisWorkout.createAt = now,
-        thisWorkout.updateAt = now
+        thisSession.name = form.data['name'],
+        thisSession.equipment = form.data['equipment'],
+        thisSession.description = form.data['description'],
+        thisSession.startTime = form.data['startTime'],
+        thisSession.endTime = form.data['endTime'],
+        thisSession.createAt = now,
+        thisSession.updateAt = now
         db.session.commit()
-        return thisWorkout.to_dict()
+        return thisSession.to_dict()
 
     return {'errors': validation_errors_to_error_messages(form.errors)}, 400
 
 #Delete a route
-@workout_routes.route('/<int:id>', methods = ['DELETE'])
+@session_routes.route('/<int:id>', methods = ['DELETE'])
 @login_required
-def delete_workout(id):
+def delete_session(id):
 
-    thisWorkout = Workout.query.get(id)
-    if thisWorkout is None:
-        return {'errors': 'Workout not found'}, 404
+    thisSession = Session.query.get(id)
+    if thisSession is None:
+        return {'errors': 'Session not found'}, 404
 
-    if thisWorkout.userId != current_user.id:
+    if thisSession.userId != current_user.id:
         return{'errors': 'Unauthorized'}, 403
 
 
-    db.session.delete(thisWorkout)
+    db.session.delete(thisSession)
     db.session.commit()
     return ("Successfully deleted!")
