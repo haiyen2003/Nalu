23d7c7cdae5e711933c2c06c656f35b92561b3f1
hai yen <nhyen.93@gmail.com>
2022-10-31 07:06:04 -0700

    commit delete spot

--- react-app/src/components/routes/SpotDetail.js
+++ react-app/src/components/routes/SpotDetail.js
@@ -12,6 +12,7 @@ function SpotDetail() {
     const history = useHistory()
     const user = useSelector((state) => state.session.user);
     const thisSpot = useSelector((state) => state.spot[spotId])
+    const isOwner = user?.id === thisSpot?.userId;
 
     useEffect(() => {
         dispatch(thunkGetOneSpot(spotId))
@@ -24,18 +25,23 @@ function SpotDetail() {
     if (!thisSpot) return null;
 
     return (
-        <div className = 'spot-container'>
-            <div className = 'spot-name'>Spot Name: {thisSpot.name}</div>
-            <div className = 'spot-description'>Description: {thisSpot.description}</div>
-            <div className = 'spot-lat'>Latitude: {thisSpot.lat}</div>
-            <div className = 'spot-lng'>Longtitude: {thisSpot.lng}</div>
-            <div className = 'spot-state'>State: {thisSpot.state}</div>
-            <div className = 'spot-level'>Level: {thisSpot.difficulty}</div>
-            <div className = 'spot-img-container'> <img className = 'spot-img' src = {thisSpot.staticUrl}></img></div>
+        <div className='spot-container'>
+            <div className='spot-name'>Spot Name: {thisSpot.name}</div>
+            <div className='spot-description'>Description: {thisSpot.description}</div>
+            <div className='spot-lat'>Latitude: {thisSpot.lat}</div>
+            <div className='spot-lng'>Longtitude: {thisSpot.lng}</div>
+            <div className='spot-state'>State: {thisSpot.state}</div>
+            <div className='spot-level'>Level: {thisSpot.difficulty}</div>
+            <div className='spot-img-container'> <img className='spot-img' src={thisSpot.staticUrl}></img></div>
 
+            <div className='button-container'>
+                {!isOwner &&
+                    <p className='notice'>You are not an creator of this spot</p>}
+                {isOwner &&
+                    <button className='button' onClick={() => handleDelete(spotId)}>Delete Spot</button>}
 
+            </div>
         </div>
     )
 }
-
 export default SpotDetail
