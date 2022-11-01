4c16ecc60a0dcccc0a7f99c96abb182148b652b7
hai yen <nhyen.93@gmail.com>
2022-11-01 06:56:09 -0700

    commit more seed file for session - prepare for delete

--- app/seeds/sessions.py
+++ app/seeds/sessions.py
@@ -11,7 +11,29 @@ def seed_sessions():
                         endTime = '9:25 AM'
                     )
 
+    session2 = Session (
+                        name='Dust Patrol at Waikiki',
+                        userId=1,
+                        spotId = 1,
+                        description='Waikiki this afternoon is glassy with light off-shore wind, water temperature around 70 degree, low tide at 9AM',
+                        image = 'https://d3duq8huj13nhl.cloudfront.net/uploads/surfing-queens-hawaii.jpg',
+                        startTime = '6:05 PM',
+                        endTime = '7:25 PM'
+                    )
+
+    session3 = Session (
+                        name='Dawn Patrol at Pacifica',
+                        userId=1,
+                        spotId = 1,
+                        description='Pacifica this morning is glassy with light off-shore wind, water temperature around 70 degree, low tide at 9AM',
+                        image = 'https://d3duq8huj13nhl.cloudfront.net/uploads/diamond-head-surfing.jpg',
+                        startTime = '6:05 AM',
+                        endTime = '9:25 AM'
+                    )
+
     db.session.add(session1)
+    db.session.add(session2)
+    db.session.add(session3)
     db.session.commit()
 
 def undo_sessions():
