0effe9ab4acf26d2941ac1f14278b40229ce23ab
hai yen <nhyen.93@gmail.com>
2022-11-04 13:32:51 -0700

    commit

--- react-app/src/components/sessions/SessionList.js
+++ react-app/src/components/sessions/SessionList.js
@@ -4,11 +4,13 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkGetAllSessions } from '../../store/workout';
 import './Session.css';
-import {placeholder} from '../../assets/placehold.jpg'
+import placeholder from '../../assets/placehold.jpg'
 
 const SessionList = () => {
     const dispatch = useDispatch();
     const data = useSelector((state) => state.sessions)
+    const history = useHistory();
+    const user = useSelector(state => state.session.user)
     console.log(data, 'THIS IS DATA =====')
     let sessions;
 
@@ -27,18 +29,26 @@ const SessionList = () => {
                 {sessions.map((session) => {
                     return (
                         <div className='list-one-container'>
-                            <div className='spotlist-name _input'>Name: {session.name}</div>
+                            <div className ='left-container'>
+                                <div className='user-info'>
+                                    <div className ='user-name'>{session.createdBy.firstName} {session.createdBy.lastName}</div>
+                                    <div className ='user-name'>{session.createAt}</div>
+                                </div>
+                            </div>
+                            <div className='spotlist-name _input'> {session.name}</div>
                             <div className='spotlist-description _input'>Description: {session.description}</div>
-                            <div className='spotlist-state _input'>Image: {session.image}</div>
                             <div className='spotlist-level _input'>Equipment: {session.equipment}</div>
                             <div className='spotlist-level _input'>Start Time: {session.startTime}</div>
                             <div className='spotlist-level _input'>End Time: {session.endTime}</div>
                             <div className = 'spotlist-img-container'><img className = 'spot-img' src ={session.spot.staticUrl}></img></div>
                             <div className='spotlist-img-container'>
                             <img className='session-img' src={session.image}></img>
-                            <img className='session-img' src = {placeholder}></img>
-                            <img className='session-img' src = {placeholder}></img>
-
+                            </div>
+                            <div className = 'spot-buttons'>
+                            <button className = 'spot-log-button' onClick={ () => {history.push(`/sessions/${session.id}`)}}>Session Detail</button>
+                            {user.id == session.userId &&
+                            <button className = 'spot-log-button' onClick={ () => {history.push(`/sessions/${session.id}/edit`)}}>Edit Session</button>
+                            }
                             </div>
                         </div>
                     )
