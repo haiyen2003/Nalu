09c52683ac9e353109150f7a2f51aba63c865e90
hai yen <nhyen.93@gmail.com>
2023-01-06 17:28:22 -0800

    commit Comment changes

---
+++ app/api/comment_routes.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from app.forms import CommentForm
+from app.models import Session, db, User, Spot
+from app.api.auth_routes import validation_errors_to_error_messages
+
+session_routes = Blueprint('sessions', __name__)
+now = datetime.now()
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .spot_form import SpotForm
 from .session_form import SessionForm
+from .comment_form import CommentForm
